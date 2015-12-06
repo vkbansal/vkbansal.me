b5497dc3d78e94375e5e21e07e7a942a50aed54d
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-12-06 16:42:00 +0530

    Add recent posts to home page

--- src/_js/main.js
+++ src/_js/main.js
@@ -7,4 +7,31 @@
       nav.classList.toggle("open");
       document.body.classList.toggle("open");
     });
+
+    if (document.body.classList.contains("home")) {
+        let request = new XMLHttpRequest();
+        request.open("GET", "/blog/recent.json", true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                let data = JSON.parse(request.responseText),
+                    homePageRecent = document.querySelector("#latest-blog-posts .blog-articles");
+
+                if (homePageRecent) {
+                    homePageRecent.innerHTML = data.reduce((prev, post) => (
+                        `${prev}<div class="article">
+                            <a href="${post.permalink}">
+                                <p class="meta">${post.date}</p>
+                                <h2>${post.title}</h2>
+                                <p>${post.description}</p>
+                            </a>
+                        </div>`
+                    ), "");
+                }
+
+            }
+        }
+
+        request.send();
+    }
 })();
