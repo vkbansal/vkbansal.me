bb1befbac0bba369e3a54ea7cdb02f0c891daaf8
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-12-22 09:56:12 +0530

    Add ajax json for tags

--- scripts/plugins/tags.js
+++ scripts/plugins/tags.js
@@ -25,58 +25,80 @@ module.exports = function({posts, options, add, template, data}) {
         return `${options.posts.tags_pagination.replace(":num", num).replace(":tag", tag)}${options.site.pretty_url ? "/index" : "" }.html`
     }
 
+    function getAjaxPageNumLink(num, tag) {
+        return `${options.posts.tags_pagination.replace(":num", num).replace(":tag", tag)}${options.site.pretty_url ? "/index" : "" }.json`
+    }
+
     function cleanPath(path) {
         return options.site.pretty_url ? path.replace(/index\.html$/, "") : path;
     }
 
     _.each(tags, (group, tag) => {
         let pages = _.chunk(group, options.posts.limit),
-        indexPath = `${options.posts.tags_dir.replace(":tag", tag)}${options.site.pretty_url ? "/index" : "" }.html`,
-        indexPage = pages.shift();
+            indexPath = `${options.posts.tags_dir.replace(":tag", tag)}${options.site.pretty_url ? "/index" : "" }.html`,
+            ajaxPath = `${options.posts.tags_dir.replace(":tag", tag)}${options.site.pretty_url ? "/" : "-" }index.json`,
+            indexPage = pages.shift(),
+            indexPageData = _.merge({
+                posts: indexPage,
+                pages: {
+                    total: pages.length + 1,
+                    current: 1
+                },
+                links: {
+                    prev: false,
+                    next: pages.length > 0 ? cleanPath(getPageNumLink(2, tag)) : false
+                },
+                tag_applied: tag
+            }, data);
+
+
 
         //Index Page
         add(
             utils.createNewFile(
                 path.join(...indexPath.split("/")),
-                template.render(
-                    LAYOUT,
-                    _.merge({
-                        posts: indexPage,
-                        pages: {
-                            total: pages.length + 1,
-                            current: 1
-                        },
-                        links: {
-                            prev: false,
-                            next: pages.length > 0 ? cleanPath(getPageNumLink(2, tag)) : false
-                        },
-                        tag_applied: tag
-                    }, data)
-                )
+                template.render(LAYOUT, indexPageData)
+            )
+        );
+
+        indexPageData.ajax = true;
+
+        add(
+            utils.createNewFile(
+                path.join(...ajaxPath.split("/")),
+                template.render(LAYOUT, indexPageData)
             )
         );
 
         //Pagination pages >= 2
         pages.forEach((page, i) => {
-            let num = i + 2;
+            let num = i + 2,
+                pageData = _.merge({
+                    posts: page,
+                    pages: {
+                        total: pages.length + 1,
+                        current: num,
+                    },
+                    links: {
+                        prev: num === 2 ? cleanPath(indexPath) : cleanPath(getPageNumLink(num - 1, tag)),
+                        next: pages.length - 1 > i ? cleanPath(getPageNumLink(num + 1, tag)) : false
+                    },
+                    tag_applied: tag
+                }, data);
+
             add(
                 utils.createNewFile(
                     path.join(...getPageNumLink(num).split("/")),
-                    template.render(
-                        LAYOUT,
-                            _.merge({
-                            posts: page,
-                            pages: {
-                                total: pages.length + 1,
-                                current: num,
-                            },
-                            links: {
-                                prev: num === 2 ? cleanPath(indexPath) : cleanPath(getPageNumLink(num - 1, tag)),
-                                next: pages.length - 1 > i ? cleanPath(getPageNumLink(num + 1, tag)) : false
-                            },
-                            tag_applied: tag
-                        }, data)
-                    )
+                    template.render(LAYOUT, pageData)
+                )
+            );
+
+            pageData.ajax = true;
+
+            add(
+                utils.createNewFile(
+                    path.join(...getAjaxPageNumLink(num).split("/")),
+                    template.render(LAYOUT, pageData)
                 )
             );
         })
