04d6fadebb1fa00d62594c58ac2f7502b0f291f9
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-03-04 12:30:08 +0530

    update markdown-loader

--- scripts/webpack/markdown-loader.js
+++ scripts/webpack/markdown-loader.js
@@ -1,10 +1,24 @@
-const postUtils = require('../utils/posts');
+const frontMatter = require('front-matter');
+const markdown = require('markdown-it');
+const mathjax = require("markdown-it-mathjax");
+const decorate = require("markdown-it-decorate");
+const span = require("../markdown-it/markdown-it-span");
+const svgInline = require("../markdown-it/markdown-it-inline-svg")
+
+const md = markdown({
+    html: true
+}).use(mathjax)
+    .use(decorate)
+    .use(span)
+    .use(svgInline);
 
 module.exports = function (content) {
     // this.cacheable();
+    const meta= frontMatter(content);
+    const body = md.render(meta.body);
+    const result = Object.assign({}, meta.attributes, { body });
 
-    const data = postUtils.processPostContent(content);
-    this.value = data;
+    // this.value = result;
 
-    return `export default ${JSON.stringify(data)}`;
+    return `export default ${JSON.stringify(result)}`;
 };
