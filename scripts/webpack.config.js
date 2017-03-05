1c5c5c291250c5909474f11b99993adf139129b8
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-03-05 11:19:43 +0530

    update webpack to load css

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
+
 const webpack = require('webpack');
+const Extract = require('extract-text-webpack-plugin');
+
 const settings = require('./settings');
 const babelRC = require('./settings/babelrc').webpack;
 
@@ -7,6 +10,9 @@ const PROD = process.env.NODE_ENV === 'production';
 
 const config = {
     context: __dirname,
+    entry: {
+        static: ['./app.js']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, '../public'),
@@ -31,6 +37,42 @@ const config = {
                 include: [
                     path.resolve(__dirname, '..')
                 ]
+            }, {
+                test: /\.scss$/,
+                use: Extract.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        query: {
+                            modules: true,
+                            localIdentName: PROD ? '[hash:base64:6]'  : '[name]_[local]_[hash:base64:6]',
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        query: {
+                            includePaths: [
+                                path.resolve(__dirname, '../node_modules/'),
+                                path.resolve(__dirname, '../src/styles/')
+                            ],
+                            sourceMap: true
+                        }
+                    }]
+                }),
+                include: [
+                    path.resolve(__dirname, '../src')
+                ]
+            }, {
+                test: /\.(svg|png|jpg|jpeg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000,
+                        name: PROD
+                                ? 'img/[name]_[hash:6].[ext]'
+                                : 'img/[name].[ext]'
+                    }
+                }]
             }
         ]
     },
@@ -53,6 +95,12 @@ const config = {
                     ? JSON.stringify('production')
                     : JSON.stringify('development')
             }
+        }),
+        new Extract({
+            filename: PROD
+                ? 'css/bundle.[contenthash:6].css'
+                : 'css/bundle.css',
+            allChunks: true
         })
     ]
 };
