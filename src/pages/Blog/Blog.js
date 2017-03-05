356c9decc43af5ae7aca3c2a1ec943c59045ce2f
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-03-06 00:01:19 +0530

    Add few pages

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
-export default function Home() {
+import Page from 'src/components/Page';
+import ArticlePreview from 'src/Components/ArticlePreview';
+import Pagination from 'src/Components/Pagination';
+import settings from 'settings.yml';
+
+import styles from './Blog.scss';
+
+export default function Blog(props) {
+    const limit = settings.blog.postsLimit;
+    const pages = props.posts.length;
+    const numPages = Math.ceil(pages / limit);
+    let page = props.match.params.num || 1;
+
+    page = parseInt(page, 10);
+    const skip = (page - 1) * limit;
+
+    const url = (page) => page === 1 ? '/blog/' : settings.blog.paginationUrl.replace(':num', page);
+
     return (
-        <div>
-            Blog
-        </div>
+        <Page {...props} className={styles['blogs-list']}>
+           <section className='container'>
+               <div className={styles['articles']}>
+                    {props.posts.slice(skip, skip + limit).map((post, i) => (
+                        <ArticlePreview key={i} post={post} className={styles['blog-page-article']}
+                            showTags />
+                    ))}
+               </div>
+                <Pagination pages={numPages} currentPage={page} url={url} />
+            </section>
+        </Page>
     );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
-export default function Home() {
+import Page from 'src/components/Page';
+import ArticlePreview from 'src/Components/ArticlePreview';
+
+import banner from './web-design-banner.svg';
+import styles from './Home.scss';
+
+import settings from 'settings.yml';
+
+export default function Home(props) {
     return (
-        <div>
-            Home
-        </div>
+        <Page {...props} className={styles['home-page']}>
+            <section className={styles['intro']}>
+                <div className='container'>
+                    <div className={styles['text']}>
+                        <h1>Hello, I'm Vivek.</h1>
+                        <h2>A frontend web <span id="intro-type">developer</span></h2>
+                    </div>
+                    <object className={styles['banner']} type='image/svg+xml' data={banner}/>
+                </div>
+            </section>
+            <section className='container'>
+                <h2>Latest posts</h2>
+                {props.posts.slice(0, settings.homepage.postsLimit).map((post, i) => (
+                    <ArticlePreview key={i} post={post} />
+                ))}
+            </section>
+        </Page>
     );
 }
