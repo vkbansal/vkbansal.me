4e4ab2faad1af69cbec70aa74f2fc2a2dd679b94
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-03-08 08:12:03 +0530

    remove custom definitions

--- scripts/webpack/markdown-loader.js
+++ scripts/webpack/markdown-loader.js
@@ -2,16 +2,12 @@ const frontMatter = require('front-matter');
 const markdown = require('markdown-it');
 const mathjax = require("markdown-it-mathjax");
 const decorate = require("markdown-it-decorate");
-const span = require("../markdown-it/markdown-it-span");
-const svgInline = require("../markdown-it/markdown-it-inline-svg")
 const htmlLoader = require('html-loader');
 
 const md = markdown({
     html: true
 }).use(mathjax)
-    .use(decorate)
-    .use(span)
-    .use(svgInline);
+    .use(decorate);
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
