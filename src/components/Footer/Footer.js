88f9ef6816fc9f5b3e72a50c2e901c193d7abeaf
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-03-26 23:33:00 +0530

    update Footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,31 +4,38 @@ import cx from 'classnames';
 import * as Icons from 'src/components/Icons';
 import settings from 'settings.yml';
 
-import styles from './Footer.scss';
+import $ from './Footer.scss';
 
 const { author, social } = settings;
 const footerLinks = social.filter((link) => link[1].footer);
-
-const captalize = (word) => word.charAt(0).toUpperCase() + word.slice(1);
+const footerBreak = Math.ceil(footerLinks.length / 2);
 
 export default function Footer() {
-    return (
-        <footer className={styles['footer']}>
-            <div className={cx('container', styles['container'])}>
-                <p className={styles['copyright']}>
-                    Copyright &copy; {(new Date()).getUTCFullYear()} {author.name}. All rights reserved.
-                </p>
-                <div className={styles['social']}>
-                    {footerLinks.map(([name, media], i) => {
-                        const Icon = Icons[captalize(name)];
+    const renderFooterLinks = ([name, media], i) => (
+        <a key={name} href={media.link} title={media.name}
+            className={cx($['link'], $[name])}>
+            {media.name}
+        </a>
+    );
 
-                        return (
-                            <a key={name} href={media.link} title={media.name}
-                                className={cx(styles['link'], styles[name])}>
-                                <Icon />
-                            </a>
-                        )
-                    })}
+    return (
+        <footer className={$['footer']}>
+            <div className={cx('container', $['container'])}>
+                <div className='col-8-12'>
+                    <h4>Find me on internet</h4>
+                    <div className={$['social']}>
+                        <div className='col-6-12'>
+                            {footerLinks.slice(0, footerBreak).map(renderFooterLinks)}
+                        </div>
+                        <div className='col-6-12'>
+                            {footerLinks.slice(footerBreak).map(renderFooterLinks)}
+                        </div>
+                    </div>
+                </div>
+                <div className='col-4-12'>
+                    <p className={$['copyright']}>
+                        &copy; {(new Date()).getUTCFullYear()} <a href={'/'}>{author.name}</a>
+                    </p>
                 </div>
             </div>
         </footer>
