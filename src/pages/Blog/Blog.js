3f9a5a8fe9df4a7a3cb200c10f4332ccd83eaa01
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-10-17 23:44:16 +0530

    fix prop-types

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Page from 'src/components/Page';
 import ArticlePreview from 'src/components/ArticlePreview';
