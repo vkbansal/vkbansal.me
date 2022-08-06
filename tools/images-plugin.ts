4a6b89e221fea6084185043eacc77c78f21678c4
Vivek Kumar Bansal <vkb0310@gmail.com>
2022-08-07 00:06:04 +0530

    chore: update packages and styles

--- tools/images-plugin.ts
+++ tools/images-plugin.ts
@@ -1,20 +1,12 @@
-import fs from 'node:fs';
 import path from 'node:path';
 import { camelCase } from 'change-case';
 import type { PluginOption } from 'vite';
-import { parse, visit, prettyPrint, type Options } from 'recast';
+import type { TransformResult } from 'rollup';
 
-const printOptions: Options = {
-	lineTerminator: '\n',
-	useTabs: false,
-	tabWidth: 2,
-	arrowParensAlways: true,
-	arrayBracketSpacing: true,
-};
-
+const HTML_REGEX = /const\s+html\s+=\s+(".*");/;
 const IMG_REGEX = /<img\s.*?(src=('|")(.*?)(\2)).*?>/g;
 
-function processHTMLContent(content: string, imgImports: string[]) {
+function processHTMLContent(content: string, imgImports: string[]): string {
 	const newContent = content.replace(IMG_REGEX, (imgTag, fullSrc, _0, src) => {
 		const variableName = camelCase(path.basename(src));
 
@@ -32,29 +24,18 @@ export function imagesPlugin(): PluginOption {
 	return {
 		name: 'images-plugin',
 		enforce: 'pre',
-		transform(code, id) {
+		transform(code: string, id: string): TransformResult {
 			if (id.endsWith('md')) {
-				const imgImports = [];
-				const ast = parse(code);
+				const imgImports: string[] = [];
+				const result = code.replace(HTML_REGEX, (_0, html) => {
+					const preprocessedHTML = JSON.parse(html) // to unescape the string
+						.replace(/(\\|\$|`)/g, '\\$1'); // escape all \, $, and `
+					const processedHTML = processHTMLContent(preprocessedHTML, imgImports);
 
-				visit(ast, {
-					visitFunctionDeclaration(path) {
-						if (path.node.id?.name === 'compiledContent') {
-							const returnStatement = path.node.body.body[0];
-
-							if (returnStatement.type === 'ReturnStatement' && returnStatement.argument) {
-								const { code } = prettyPrint(returnStatement.argument, printOptions);
-								const processedHTML = processHTMLContent(code, imgImports);
-
-								returnStatement.argument = parse(processedHTML).program.body[0];
-							}
-						}
-
-						return false;
-					},
+					return `const html = \`${processedHTML}\`;`;
 				});
 
-				const finalCode = `${imgImports.join('\n')}\n${prettyPrint(ast, printOptions).code}`;
+				const finalCode = `${imgImports.join('\n')}\n${result}`;
 
 				return {
 					code: finalCode,
