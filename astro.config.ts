8d572db964c56e5f34b9c625e78601907b680b53
Vivek Kumar Bansal <vkb0310@gmail.com>
2024-06-15 14:14:28 +0530

    update packages

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,7 @@
-import path from 'node:path';
-import fs from 'node:fs';
-import { fileURLToPath } from 'node:url';
 import { type RehypePlugin } from '@astrojs/markdown-remark';
-import type { ThemeRegistration } from 'shikiji';
-
+import type { ThemeRegistration } from 'shiki';
+import { transformerTwoslash } from '@shikijs/twoslash';
+import { transformerNotationDiff, transformerNotationHighlight } from '@shikijs/transformers';
 import { defineConfig } from 'astro/config';
 import rehypeExternalLinks from 'rehype-external-links';
 import rehypeAttrs from 'rehype-attr';
@@ -11,23 +9,17 @@ import rehypeAttrs from 'rehype-attr';
 import { imagesPlugin } from './tools/images-plugin';
 import shikiTheme from './tools/vscode-themes/noctis/minimus.json';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const CERTS_DIR = path.resolve(__dirname, './.certificates');
-const CERTS_DIR_EXISTS = fs.existsSync(CERTS_DIR);
-const SSL_KEY = CERTS_DIR_EXISTS
-	? fs.readFileSync(path.resolve(CERTS_DIR, 'localhost-key.pem'), 'utf8')
-	: null;
-const SSL_CERT = CERTS_DIR_EXISTS
-	? fs.readFileSync(path.resolve(CERTS_DIR, 'localhost.pem'), 'utf8')
-	: null;
-
 // https://astro.build/config
 export default defineConfig({
 	outDir: 'dist',
 	markdown: {
-		syntaxHighlight: 'shiki',
 		shikiConfig: {
-			theme: shikiTheme as unknown as ThemeRegistration,
+			theme: shikiTheme as ThemeRegistration,
+			transformers: [
+				transformerTwoslash({ explicitTrigger: true }),
+				transformerNotationDiff(),
+				transformerNotationHighlight(),
+			],
 		},
 		remarkRehype: {
 			allowDangerousHtml: true,
@@ -39,16 +31,6 @@ export default defineConfig({
 	},
 	server: { port: 4000 },
 	vite: {
-		server: {
-			https:
-				SSL_CERT && SSL_KEY
-					? {
-							// generate certificates using `mkcert localhost`
-							key: SSL_KEY,
-							cert: SSL_CERT,
-					  }
-					: false,
-		},
 		plugins: [imagesPlugin()],
 	},
 });
